3731a61678c27d37b99dfe79bb254047861e8d01
SanketLV <sanketlakhani80@gmail.com>
2025-08-08 12:50:34 +0530

    Refactor code structure for improved readability and maintainability

---
+++ apps/web/sentry.server.config.ts
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a9d2eb82db8f90dbcc0f961b2251a7cc@o4509807107899392.ingest.us.sentry.io/4509807139684352",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
